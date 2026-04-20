e4d05d6fe39527d7ec1659ae986d23c6c3200502
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-04-20 23:00:43 +0530

    Root - Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+// Root Component
 import react from 'react'
 import Message from './components/Message';
-class App extends react.Component{
-
-    render(){
-        return <div>
-            <h1>App Component</h1>
-            {/*  calling message components */}
-            <Message/> 
-        </div>
-    }
+import Movie from './components/Movie';
+class App extends react.Component {
+  render() {
+    return (
+      <div style={{ backgroundColor: "#1e293b", color: 'white', fontStyle : 'italic' }}>
+        <h1>App Component</h1>
+        <hr />
+        <Message />
+        <hr />
+        <Movie />
+      </div>
+    );
+  }
 }
 
 export default App;
