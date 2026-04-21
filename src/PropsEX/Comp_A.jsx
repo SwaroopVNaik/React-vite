6712d98012275519740f98900178017402ae6d63
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-04-21 22:52:46 +0530

    component_1

---
+++ src/PropsEX/Comp_A.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+// import Comp_B from './Comp_B'
+// const Comp_A = () => {
+//   let price=399;
+//   let loc=["Bangalore","Chennai"]
+  
+//   return <div>
+//             <h2>Component A</h2>
+//             <hr/>
+//             <CompB  price={price} msg={"GM"} locations={loc}/>
+//          </div>
+// }
+
+// export default Comp_A;
---
+++ src/PropsEX/Comp_B.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+// const Comp_B = (props) => {
+//   return <div>
+//             <h2>Component B</h2>
+//             {/* converting Objects to String */}
+//             <pre>{JSON.stringify(props)}</pre>
+
+//             <h3>Price:{props.price}</h3>
+//             <h3>Wish message:{props.msg}</h3>
+//             <h3>Location:{props.locations[1]}</h3>
+//          </div>
+// }
+// export default Comp_B;
