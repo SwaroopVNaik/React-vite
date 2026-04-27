fc05dcfdd9ead3c914b3ed696921c898025a6dbc
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-04-27 22:31:52 +0530

    states

--- States/Message_State.jsx
+++ States/Message_State.jsx
@@ -1,18 +1,27 @@
-import React,{ useState } from "react"
-let Message_State =()=>{
-    let [Mes,setMes]=useState("hello");
-    let gmHandler=()=>{
-        setMes("Good Morning")
-    }
-    let gnHandler=()=>{
-        setMes("Good Night")
-    }
-    return <div>
-        <h3>Message Component Arrow function</h3>
-        <h3> Message value:{Mes}</h3>
-        <button onClick={gmHandler}>GM</button>
-        <button onClick={gnHandler}>GN</button>
-    </div>
+import { useState } from "react";
 
+let Message=()=>{
+    let [msg,setMsg] = useState("Hello")
+    let gmHandler = ()=>{
+        setMsg("Good Morning")
+    }
+     let gaHandler = ()=>{
+        setMsg("Good Afternoon")
+    }
+     let geHandler = ()=>{
+        setMsg("Good Evening")
+    }
+     let gnHandler = ()=>{
+        setMsg("Good Night")
+    }
+
+    return <div>
+                <h2>Message Component</h2>
+                <h3>Value:{msg}</h3>
+                <button onClick={gmHandler}>GM</button>
+                <button onClick={gaHandler}>GA</button>
+                <button onClick={geHandler}>GE</button>
+                <button onClick={gnHandler}>GN</button>
+            </div>
 }
-export default Message_State;
+export default Message;
