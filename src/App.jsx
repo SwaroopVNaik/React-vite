ee08cce49978a35f0c95515dacaf98fadf54dc56
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-04-27 22:31:07 +0530

    Main_Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
+// import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import React from "react";
-import User from "./Users/User";
-import Message from "../States/Message";
-import Message_State from "../States/Message_State";
-import Counter from "../States/Counter";
-class App extends React.Component{
-    
-    render(){
-        return <div>
-        <h1>App Component</h1>
-        <hr/>
-        <User/>
-        <hr />
-        <Message />
-        <hr />
-        <Message_State />
-        <hr />
-        <Counter />
+// import User from "./Users/User";
+// import Message from "../States/Message";
+// import Message_State from "../States/Message_State";
+// import Counter from "../States/Counter";
+import Navbar from "./components/Navbar";
+
+let App = () =>{
+
+    return <div>
+        <Navbar />
     </div>
-}
 
 }
 
