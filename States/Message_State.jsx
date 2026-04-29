2e4fd8b158b4224f8db696003cb49edea8e6fd1c
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-04-29 12:44:33 +0530

    States

--- States/Message_State.jsx
+++ States/Message_State.jsx
@@ -2,26 +2,18 @@ import { useState } from "react";
 
 let Message=()=>{
     let [msg,setMsg] = useState("Hello")
-    let gmHandler = ()=>{
-        setMsg("Good Morning")
-    }
-     let gaHandler = ()=>{
-        setMsg("Good Afternoon")
-    }
-     let geHandler = ()=>{
-        setMsg("Good Evening")
-    }
-     let gnHandler = ()=>{
-        setMsg("Good Night")
+    let updateHandler = (value) => {
+        console.log(value);
+        setMsg(value)
     }
 
     return <div>
                 <h2>Message Component</h2>
                 <h3>Value:{msg}</h3>
-                <button onClick={gmHandler}>GM</button>
-                <button onClick={gaHandler}>GA</button>
-                <button onClick={geHandler}>GE</button>
-                <button onClick={gnHandler}>GN</button>
+                <button onClick={updateHandler.bind(null, "GM")}>GM</button>
+                <button onClick={updateHandler.bind(null, "GA")}>GA</button>
+                <button onClick={updateHandler.bind(null, "GE")}>GE</button>
+                <button onClick={updateHandler.bind(null, "GN")}>GN</button>
             </div>
 }
 export default Message;
